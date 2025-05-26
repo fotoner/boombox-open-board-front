b4556f0fe4a731b8a03ef66fa93a6e657753db2d
Hyeonjae Nam <jaemuon5582@gmail.com>
2025-05-27 04:49:05 +0900

    feat: 메타데이터 업데이트 - 설명 및 키워드 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,22 +9,19 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "오타쿠 붐박스 - 오픈 테마 보드",
-  description:
-    "참가자들이 행사 주제를 제안하고 트위터(X)를 통해 확산시키는 커뮤니티 보드",
-  keywords: ["오타쿠", "애니메이션", "음악", "테마", "커뮤니티"],
+  description: "붐박스의 테마를 직접 정해보세요!!",
+  keywords: ["오타쿠", "애니메이션", "음악", "테마", "디제이", "커뮤니티"],
   authors: [{ name: "오타쿠 붐박스" }],
   openGraph: {
     title: "오타쿠 붐박스 - 오픈 테마 보드",
-    description:
-      "참가자들이 행사 주제를 제안하고 트위터(X)를 통해 확산시키는 커뮤니티 보드",
+    description: "붐박스의 테마를 직접 정해보세요!!",
     type: "website",
     locale: "ko_KR",
   },
   twitter: {
     card: "summary_large_image",
     title: "오타쿠 붐박스 - 오픈 테마 보드",
-    description:
-      "참가자들이 행사 주제를 제안하고 트위터(X)를 통해 확산시키는 커뮤니티 보드",
+    description: "붐박스의 테마를 직접 정해보세요!!",
   },
   generator: "otaku-bombox",
 };
